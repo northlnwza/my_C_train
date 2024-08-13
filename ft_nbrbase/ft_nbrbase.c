7a4324745e8d958402add8c0928f2eaede2f89d1
Your Name <thanakrit.0127@gmail.com>
2024-08-13 23:13:31 +0700

    add ft_nbrbase

--- ft_nbrbase/ft_nbrbase.c
+++ ft_nbrbase/ft_nbrbase.c
@@ -15,13 +15,22 @@ int	ft_strl(char *str)
 
 void	ft_nbrbase(char *baseform, int dec_in)
 {
+	int	bfl;
+
 	bfl = ft_strl(baseform);
+	if (dec_in / bfl != 0)
+		ft_nbrbase(baseform, dec_in / bfl);
+	else
+		if (bfl <= dec_in)
+			write(1, &baseform[dec_in / bfl], 1);
+
+	write(1, &baseform[dec_in % bfl], 1);
 }
 
 int	atoi(char *str)
 {
 	int	nbr;
-	
+		
 	nbr  = 0;
 	while (*str)
 	{
@@ -34,5 +43,6 @@ int	atoi(char *str)
 int	main(int arg_c, char **arg_v)
 {
 	ft_nbrbase(arg_v[1], atoi(arg_v[2]));
+	write(1, "\n", 1);
 	return (0);
 }
