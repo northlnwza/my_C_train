d0ba0505a9c476752c983ba30ee0c0c464048ef3
Your Name <thanakrit.0127@gmail.com>
2024-09-12 23:10:33 +0700

    strinput

---
+++ strinput/strinput.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LIMIT 80
+
+char	*str_input(void)
+{
+	int	i;
+	char	*buffer;
+	int	ch;
+
+	buffer = (char *)malloc(sizeof(char) * (LIMIT + 1));
+	i = 0;	
+	while (i < LIMIT && ch != EOF && ch != '\n')
+	{
+		ch = getchar();
+		buffer[i] = ch; 
+		i++;
+	}
+	buffer[i] = 0;
+	return buffer;
+}
+
+int	main(void)
+{
+	char	*buffer;
+
+	printf("Input message  : ");
+	buffer = str_input();
+	printf("Output message : %s\n", buffer);
+	free(buffer);
+	return (0);
+}
+
